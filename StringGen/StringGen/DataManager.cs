4740b572607da9439128c9e0d72a0cc25a713007
petruskiendys@gmail.com <petruskiendys@gmail.com@e2d112ac-75ef-a29d-1ba8-23a7df960a66>
2015-01-15 18:17:50 +0000

    - StringGen -- A simple implementation of StringGen, more features may be added as needed

---
+++ StringGen/StringGen/DataManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StringGen
+{
+	class DataManager
+	{
+
+		internal string LoadFile(string path)
+		{
+			FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+			StreamReader reader = new StreamReader(file);
+			string res = reader.ReadToEnd();
+			reader.Close();
+			file.Close();
+
+			return res;
+		}
+
+		internal void SaveFile(string path, string content)
+		{
+			FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write);
+			StreamWriter writer = new StreamWriter(file);
+
+			// NOTE: optional formatting of the output file can be done here...
+			writer.WriteLine(content);
+
+			writer.Close();
+			file.Close();
+		}
+
+	}
+}
---
+++ StringGen/StringGen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringGen
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StringGen sg = new StringGen();
+			sg.Run();
+		}
+	}
+}

6d0e873c0ede412a4da17e91f8c20a1c408f852d
petruskiendys@gmail.com <petruskiendys@gmail.com@e2d112ac-75ef-a29d-1ba8-23a7df960a66>
2015-01-15 18:17:50 +0000

    - StringGen -- A simple implementation of StringGen, more features may be added as needed

---
+++ trunk/StringGen/StringGen/Processor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringGen
+{
+	class Processor
+	{
+		/// <summary>
+		/// Generates a string given a start number and an end number, surrounded with specified strings.
+		/// </summary>
+		/// <param name="startNumber"></param>
+		/// <param name="endNumber"></param>
+		/// <param name="leftSurround"></param>
+		/// <param name="rightSurround"></param>
+		/// <returns></returns>
+		internal string generateStringIterativeFormat(int startNumber, int endNumber, string leftSurround, string rightSurround)
+		{
+			return generateStringIterativeFormat(startNumber, endNumber, leftSurround, rightSurround, "");
+		}
+
+		/// <summary>
+		/// Generates a string given a start number and an end number, surrounded with specified strings and separated with specified separator.
+		/// </summary>
+		/// <param name="startNumber"></param>
+		/// <param name="endNumber"></param>
+		/// <param name="leftSurround"></param>
+		/// <param name="rightSurround"></param>
+		/// <param name="separator"></param>
+		/// <returns></returns>
+		private string generateStringIterativeFormat(int startNumber, int endNumber, string leftSurround, string rightSurround, string separator)
+		{
+			if (startNumber > endNumber)
+				throw new ArgumentException("startNumber is bigger than endNumber");
+
+			string res = "";
+
+			for (int i = startNumber; i <= endNumber; i++)
+			{
+				res += leftSurround + i + rightSurround + separator;
+			}
+
+			return res;
+		}
+	}
+}
---
+++ trunk/StringGen/StringGen/StringGen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringGen
+{
+	class StringGen
+	{
+
+		internal void Run()
+		{
+			DataManager dm = new DataManager();
+			Processor proc = new Processor();
+
+			string str = proc.generateStringIterativeFormat(10, 30, "[", "]");
+			dm.SaveFile("../../out/output.txt", str);
+		}
+	}
+}
